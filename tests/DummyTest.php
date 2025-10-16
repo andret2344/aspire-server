698149e913ed5f0e9a877ec8b646867217750ebd
Andret2344 <andret2344@gmail.com>
2025-10-16 10:14:15 +0200

    Added phpunit
    Took 11 minutes

---
+++ tests/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DummyTest extends WebTestCase
+{
+	public function testDummy(): void
+	{
+		$this->assertTrue(true);
+	}
+}
