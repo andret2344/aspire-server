4e3ad324b0889482541b307051613b2d0639d376
Andret2344 <andret2344@gmail.com>
2026-01-12 20:46:25 +0100

    EX: Removed commented-out code
    Took 3 minutes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-#echo "Waiting for database (and creating it if missing)..."
-#READY=0
-#for _ in $(seq 1 60); do
-#  if php bin/console doctrine:database:create --if-not-exists >/dev/null 2>&1; then
-#    READY=1
-#    break
-#  fi
-#  sleep 1
-#done
-#
-#[ "$READY" -eq 1 ] || { echo "DB not ready"; exit 1; }
-
 echo "Creating database..."
 php bin/console doctrine:database:create --if-not-exists
 
